cfbe41dd18aac46ce77b54613f7e100e5630e700
akivasoya <30340491+akivasoya@users.noreply.github.com>
2021-09-25 15:21:45 +0530

    Delete Newsletter.php

--- Newsletter.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (isset($_REQUEST["email"])) {
-
-        $msg = "News Latter Page \n";
-		$msg .= "Mail ID : ".$_REQUEST['email']."\n";
-		mail("info@ekakshoverseas.com","Contact From Dehydrated Website",$msg,$head);
-		header('Location: dehydreated-onions-garlic.html');
-}
-?>
